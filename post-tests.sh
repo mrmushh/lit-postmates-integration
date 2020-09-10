f59cbea8d2e6f89472782f605c88ae557bbc8d87
muchohot22 <muchohot22@gmail.com>
2020-09-10 12:20:39 -0400

    Testing files for postmates api functionality

---
+++ post-tests.sh
@@ -0,0 +1,11 @@
+FILE=new-delivery.json
+CUSTOMER_ID=$(cat customer-id)
+API_KEY=$(cat sandbox-key)
+
+# Create a postmates delivery from a JSON file in current directory
+
+# curl --user $API_KEY: -H "Content-Type: application/json" -d @$FILE  https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries
+
+# Get postmates delivery history
+
+curl --user $API_KEY: https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries | jq '. | {data}'
