3caf059f84333f59277c7bb94d8ab2edada38784
muchohot22 <muchohot22@gmail.com>
2020-09-11 13:41:14 -0400

    connected menu on google sheets to app

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 
 var queue = [];
+var orders = [];
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -14,20 +16,29 @@ module.exports = router;
 router.post("/", (req, res) => {
   res.status(200);
   res.send();
+  //do delivery id check here (we only want one post)
   queue.push(req.body);
 })
 
 /* Get useful data from post request data. */
 setInterval( function () {
   if(queue.length > 0) {
-    console.log("queue has " + queue.length + " items in it");
+    console.log("data queue has " + queue.length + " items in it");
     let current_order = getOrderInfo(queue.shift());
-    console.log(current_order);
+    orders.push(current_order);
   }
-  else {
-    //console.log("queue is empty");
-  }
+  //console.log("queue is empty");
 }, 3000 );
+
+/* Add printer information to order objects */
+setInterval( function () {
+  if(orders.length > 0) {
+    assignItemsToPrinters(orders);
+  }
+  //console.log("orders is empty");
+}, 3000 );
+
+/*Helper functions*/
 
 function getOrderInfo(request_body) {
   let order = {};
@@ -36,7 +47,48 @@ function getOrderInfo(request_body) {
   order.customer_name = request_body.data.dropoff.name; 
   order.phone_number = request_body.data.dropoff.phone_number;
   order.all_items = request_body.data.manifest_items;
-  order.placed_at = request_body.data.created;
-  order.ready_by = request_body.data.pickup_ready;
+  order.placed_at = new Date(request_body.data.created).toString();
+  order.ready_by = new Date(request_body.data.pickup_ready).toString();
   return order;
+}
+function readMenu() {
+  try {
+    const jsonString = fs.readFileSync('./menu.json')
+    const menu = JSON.parse(jsonString)
+    return menu
+  } catch(err) {
+    console.log(err)
+    return
+  }
+}
+function getPrinters(item_name){
+  let menu = readMenu();
+  let printer_ids = ''
+  for(let i = 0; i < menu.length; i++){
+    if(item_name == menu[i].name){
+      return menu[i].printers;
+    }
+  }
+}
+function assignItemsToPrinters(orderQueue){
+  for(let i = 0; i < orderQueue.length; i++){
+    if(typeof orderQueue[i].foodrun_items === 'undefined'){
+      orderQueue[i].foodrun_items = orderQueue[i].all_items;
+      orderQueue[i].app_items = [];
+      orderQueue[i].entree_items = [];
+      orderQueue[i].dessert_items = [];
+      for(let item = 0; item < orderQueue[i].all_items.length; item++){
+        if(getPrinters(orderQueue[i].all_items[item].name).includes("A")){
+          orderQueue[i].app_items.push(orderQueue[i].all_items[item]);
+        }
+        if(getPrinters(orderQueue[i].all_items[item].name).includes("E")){
+          orderQueue[i].entree_items.push(orderQueue[i].all_items[item]);
+        }
+        if(getPrinters(orderQueue[i].all_items[item].name).includes("D")){
+          orderQueue[i].dessert_items.push(orderQueue[i].all_items[item]);
+        }
+      }
+      console.log(orderQueue.shift())
+    }
+  }
 }
