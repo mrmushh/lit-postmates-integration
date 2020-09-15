5e42b9021093f8105ed8292e6a42f48fe2f7ee2c
Matthew Abrahamsen <muchohot22@gmail.comm>
2020-09-14 20:47:50 -0400

    added api keys to gitignore

--- post-tests.sh
+++ post-tests.sh
@@ -1,11 +1,11 @@
 FILE=new-delivery.json
 CUSTOMER_ID=$(cat customer-id)
-API_KEY=$(cat sandbox-key)
+API_KEY=$(cat production-key)
 
 # Create a postmates delivery from a JSON file in current directory
 
-curl --user $API_KEY: -H "Content-Type: application/json" -d @$FILE  https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries | jq '.'
+# curl --user $API_KEY: -H "Content-Type: application/json" -d @$FILE  https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries | jq '.'
 
 # Get postmates delivery history
 
-# curl --user $API_KEY: https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries | jq '.data[0].manifest_items[0].name, .data[0].manifest_items[0].quantity' 
+curl --user $API_KEY: https://api.postmates.com/v1/customers/$CUSTOMER_ID/deliveries
